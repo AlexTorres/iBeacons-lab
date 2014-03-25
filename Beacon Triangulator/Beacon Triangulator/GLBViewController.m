73a2fb81fdd0d1113f6d5986f6adda802ef549b1
Mauricio Santos <mauriciosantoss@gmail.com>
2014-03-25 16:24:32 -0500

    new triangulation algorithm

--- Beacon Triangulator/Beacon Triangulator/GLBViewController.m
+++ Beacon Triangulator/Beacon Triangulator/GLBViewController.m
@@ -21,6 +21,9 @@
 
 @property(nonatomic,strong)  NSMutableDictionary *receivedBeaconDistances;
 @property(nonatomic,readonly)  CGPoint currentCoordinate;
+@property (weak, nonatomic) IBOutlet UILabel *detectedBeaconsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *distancesLabel;
+@property (weak, nonatomic) IBOutlet UILabel *closestBeaconLabel;
 
 @end
 
@@ -41,24 +44,7 @@
 -(CGPoint)currentCoordinate
 {
     if ([self.receivedBeaconDistances count] >=3){
-//        CGPoint a = [self pointForBeaconWithMinor:1];
-//        CGPoint b = [self pointForBeaconWithMinor:2];
-//        CGPoint c = [self pointForBeaconWithMinor:2];
-//        
-//        CGFloat dA = [self.receivedBeaconDistances[@1] floatValue];
-//        CGFloat dB = [self.receivedBeaconDistances[@2] floatValue];
-//        CGFloat dC = [self.receivedBeaconDistances[@3] floatValue];
-//        
-//        
-//        CGFloat W, Z, x, y, y2;
-//        W = dA*dA - dB*dB - a.x*a.x - a.y*a.y + b.x*b.x + b.y*b.y;
-//        Z = dB*dB - dC*dC - b.x*b.x - b.y*b.y + c.x*c.x + c.y*c.y;
-//        
-//        x = (W*(c.y-b.y) - Z*(b.y-a.y)) / (2 * ((b.x-a.x)*(c.y-b.y) - (c.x-b.x)*(b.y-a.y)));
-//        y = (W - 2*x*(b.x-a.x)) / (2*(b.y-a.y));
-//        y2 = (Z - 2*x*(c.x-b.x)) / (2*(c.y-b.y));
-//        
-//        y = (y + y2) / 2;
+
         
         NSMutableArray *P1 = [[NSMutableArray alloc] initWithCapacity:0];
         [P1 addObject:@([self pointForBeaconWithMinor:1].x)];
@@ -271,12 +257,21 @@
     NSArray *filteredBeacons = [self filteredBeacons:beacons];
     for (CLBeacon *beaconData in filteredBeacons) {
         self.receivedBeaconDistances[beaconData.minor] = @(beaconData.accuracy);
+        if (beaconData.proximity == CLProximityImmediate) {
+            self.iBeaconStatus.text=@"Beacon Found";
+            
+            self.positionLabel.text = [NSString stringWithFormat:@"%f, %f",self.currentCoordinate.x, self.currentCoordinate.y];
+            self.detectedBeaconsLabel.text = [[self.receivedBeaconDistances allKeys] componentsJoinedByString:@","];
+            
+            self.distancesLabel.text = [[self.receivedBeaconDistances allValues] componentsJoinedByString:@", "];
+            self.closestBeaconLabel.text = [NSString stringWithFormat:@"%@",beaconData.minor];
+            
+        }
     }
     
-    self.iBeaconStatus.text=@"Beacon Found";
     
-    self.positionLabel.text = [NSString stringWithFormat:@"%f, %f",self.currentCoordinate.x, self.currentCoordinate.y];
 }
+
 
 
 -(CGPoint) pointForBeaconWithMinor:(NSUInteger)minor
@@ -286,9 +281,10 @@
     }
     else if(minor==2){
         return CGPointMake(0, 5);
-    } else{
+    } else if(minor==3){
         return CGPointMake(3, 4.5);
     }
+    return  CGPointMake(-1, -1);
 }
 
 
